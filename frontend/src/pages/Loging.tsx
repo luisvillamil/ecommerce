f3873639d933ccc0c88df6e08d38b320860c28d6
AndresNeisa <cneisa94@gmail.com>
2024-06-17 20:35:13 -0400

    Update login page mobile style

--- frontend/src/pages/Loging.tsx
+++ frontend/src/pages/Loging.tsx
@@ -13,22 +13,60 @@ const classes = {
 };
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const response = await LoginService.getAccessToken({
+      formData: {
+        username: username,
+        password: password,
+      },
+    });
+    console.log(response);
+    // localStorage.setItem('access_token', response.access_token)
+  };
+
   return (
-    <Box sx={classes.verticalContainer}>
+    <Box sx={{ px: 1, py: 6 }}>
       <Container maxWidth="md">
-        <img src={RollingDoodle} />
-        <Typography variant="h5" sx={{ fontWeight: 700 }}>
-          Welcome back
-        </Typography>
-        <Typography variant="body1">Log in to navigate your store!</Typography>
-        <Stack spacing={3}>
-          <TextField size="small" label="User Name" fullWidth />
-          <TextField size="small" label="Password" type="password" fullWidth />
+        <Box>
+          <img src={RollingDoodle} />
+        </Box>
+
+        <Stack spacing={1} sx={{ mb: 5 }}>
+          <Typography variant="h5" sx={{ fontWeight: 700, color: "#7fbbca" }}>
+            Welcome back
+          </Typography>
+          <Typography variant="subtitle1">Log in to navigate your store!</Typography>
         </Stack>
-        <Link>Forgot Password?</Link>
-        <Button variant="contained" onClick={() => {}}>
-          Login
-        </Button>
+
+        <Stack spacing={3}>
+          <TextField
+            size="small"
+            label="User Name"
+            fullWidth
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            size="small"
+            label="Password"
+            type="password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Stack>
+        <Box sx={{ textAlign: "right", mt: 1 }}>
+          <Link>Forgot Password?</Link>
+        </Box>
+
+        <Box sx={{ width: "100%", mt: 4 }}>
+          <Button sx={{ textTransform: "none", fontSize: "1rem" }} variant="contained" onClick={handleLogin} fullWidth>
+            Login
+          </Button>
+        </Box>
       </Container>
     </Box>
   );
